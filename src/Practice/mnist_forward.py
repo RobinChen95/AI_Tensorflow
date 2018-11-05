2db5f1409e82137aa9a5b9de3624e4a68c33155a
陈国强 <407339708@qq.com>
2018-11-05 19:29:59 +0800

    实现CNN神经网络

--- src/Practice/mnist_forward.py
+++ src/Practice/mnist_forward.py
@@ -1,12 +1,17 @@
 import tensorflow as tf
 
+# 784个输入结点，输入的图片像素值，28X28，每个点是0-1之间的浮点数
 INPUT_NODE = 784
+# 10个输出，0-9
 OUTPUT_NODE = 10
+# 500个隐藏层结点
 LAYER1_NODE = 500
 
 
 def get_weight(shape, regularizer):
+    # 随机生成参数w
     w = tf.Variable(tf.truncated_normal(shape, stddev=0.1))
+    # 如果使用正则化，则把每个w的正则化计入总loss
     if regularizer != None: tf.add_to_collection('losses', tf.contrib.layers.l2_regularizer(regularizer)(w))
     return w
 
@@ -17,6 +22,7 @@ def get_bias(shape):
 
 
 def forward(x, regularizer):
+    # 参数w、b、y
     w1 = get_weight([INPUT_NODE, LAYER1_NODE], regularizer)
     b1 = get_bias([LAYER1_NODE])
     y1 = tf.nn.relu(tf.matmul(x, w1) + b1)
--- src/Practice/mnist_test.py
+++ src/Practice/mnist_test.py
@@ -5,31 +5,39 @@ from tensorflow.examples.tutorials.mnist import input_data
 import mnist_forward
 import mnist_backward
 
+# 程序间隔时间5s
 TEST_INTERVAL_SECS = 5
 
 
 def test(mnist):
+    # 复现计算图
     with tf.Graph().as_default() as g:
         x = tf.placeholder(tf.float32, [None, mnist_forward.INPUT_NODE])
         y_ = tf.placeholder(tf.float32, [None, mnist_forward.OUTPUT_NODE])
-        y = mnist_forward.forward(x, None)
 
+        # 前向传播计算出y的值
+        y = mnist_forward.forward(x, None)
+        # 实例化带滑动平均的saver对象，赋值为各自的滑动平均值
         ema = tf.train.ExponentialMovingAverage(mnist_backward.MOVING_AVERAGE_DECAY)
         ema_restore = ema.variables_to_restore()
         saver = tf.train.Saver(ema_restore)
 
+        # 计算准确率
         correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
         while True:
             with tf.Session() as sess:
+                # 加载ckpt
                 ckpt = tf.train.get_checkpoint_state(mnist_backward.MODEL_SAVE_PATH)
+                # 先判断是否已经由模型，如果有，恢复模型到当前会话，恢复参数
                 if ckpt and ckpt.model_checkpoint_path:
                     saver.restore(sess, ckpt.model_checkpoint_path)
                     global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
                     accuracy_score = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
                     print("After %s training step(s), test accuracy = %g" % (global_step, accuracy_score))
                 else:
+                    # 提示模型没有找到
                     print('No checkpoint file found')
                     return
             time.sleep(TEST_INTERVAL_SECS)
