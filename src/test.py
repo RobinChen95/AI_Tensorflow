e198cb230649fe61980ea1898fee7a171f732abc
chenguoqiang <407339708@qq.com>
2019-03-14 18:41:06 +0800

    ???

--- src/test.py
+++ src/test.py
@@ -1,18 +1,9 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from json import dumps
-from flask.ext.jsonpify import jsonify
+def fibo(n):
+    if n <= 2:
+        return 1
+    else:
+        return fibo(n - 1) + fibo(n - 2)
 
-app = Flask(__name__)
-api = Api(app)
-
-
-class Employees(Resource):
-    def get(self):
-        return {'employees'}
-
-
-api.add_resource(Employees, '/employees')
 
 if __name__ == '__main__':
-    app.run(port='5002')
+    print fibo(3)
