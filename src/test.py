048394e4756084d3feeeaff8f84c450bb754602a
RobinChen95 <407339708@pku.edu.cn>
2019-07-28 18:17:26 +0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,27 +1,31 @@
-print("physical page0 : used, virtual page0 threadID 0")
-print("physical page1 : used, virtual page1 threadID 0")
-print("physical page2 : used, virtual page3 threadID 0")
-print("physical page3 : used")
-print("physical page4 : used")
-print("physical page5 : used")
-print("physical page6 : used")
-print("physical page7 : used")
-print("physical page8 : used")
-print("physical page9 : used")
-print("physical page10 : used\n")
+import pymysql  # 导入 pymysql
+import json
+import codecs
 
-print("threadID 0 suspend")
+# 打开数据库连接
+'''
+db= pymysql.connect(host="39.106.6.6",user="haiou",
+     password="haiou",db="haiou",port=3306,cursorclass=pymysql.cursors.DictCursor)
+'''
+db = pymysql.connect(host="39.106.6.6", user="loushuai",
+                     password="loushuai", db="CompanyInfo", port=3306, cursorclass=pymysql.cursors.DictCursor)
 
-print("physical page0 : idle")
-print("physical page1 : idle")
-print("physical page2 : idle")
-print("physical page3 : idle")
-print("physical page4 : idle")
-print("physical page5 : idle")
-print("physical page6 : idle")
-print("physical page7 : idle")
-print("physical page8 : idle")
-print("physical page9 : idle")
-print("physical page10 : idle")
+# 使用cursor()方法获取操作游标
+cur = db.cursor()
 
-
+# 1.查询操作
+# 编写sql 查询语句  user 对应我的表名
+# sql = "select * from data"
+sql = "alter table data add rate varchar(2) null"
+sql2 = "select * from company_base_info limit 1,100"
+# f_res = codecs.open('rate.txt', 'w', 'utf-8')
+try:
+    #  cur.execute(sql)  #执行sql语句
+    cur.execute(sql2)
+    results = cur.fetchall()  # 获取查询的所有记录
+    cnt = 0
+    #  score_set = []
+    for item in results:
+        print(item)
+except Exception as e:
+    raise e
